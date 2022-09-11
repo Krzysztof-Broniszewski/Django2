ade6d3012f54925c27c41f96cd0524ad64f07244
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2022-09-11 17:00:14 +0200

    Added common menu

--- strony/views.py
+++ strony/views.py
@@ -7,8 +7,10 @@ def home_view(request, *args, **kwargs):
     # 
     return render(request, "dom.html", {})
 
-def kontakt(*args, **kwargs):
-    return HttpResponse("<h1>Kontakt do mnie</h1>")
+def kontakt(request, *args, **kwargs):
+    # return HttpResponse("<h1>Kontakt do mnie</h1>")
+    return render(request, "kontakt.html", {})
 
-def o_nas(*args, **kwargs):
-    return HttpResponse("<h1>Kilka słów o nas</h1>")
+def o_nas(request, *args, **kwargs):
+    # return HttpResponse("<h1>Kilka słów o nas</h1>")
+    return render(request, "onas.html", {})
