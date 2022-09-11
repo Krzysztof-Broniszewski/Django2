510450705125a4b105508b1173271f5cd5b26b3d
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2022-09-11 20:32:05 +0200

    After added the for loop

--- strony/views.py
+++ strony/views.py
@@ -8,8 +8,14 @@ def home_view(request, *args, **kwargs):
     return render(request, "dom.html", {})
 
 def kontakt(request, *args, **kwargs):
+    kontekst = {
+        "users" : ["user1", "user2", "user3", "user4"],
+        "phone_number" : 1123,
+        "numbers" : [123, 456, 789, 123]
+    }
+
     # return HttpResponse("<h1>Kontakt do mnie</h1>")
-    return render(request, "kontakt.html", {})
+    return render(request, "kontakt.html", kontekst)
 
 def o_nas(request, *args, **kwargs):
     # return HttpResponse("<h1>Kilka słów o nas</h1>")
